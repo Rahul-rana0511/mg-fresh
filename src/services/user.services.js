5033e18e44fa7b2dc206af07eddaa424fe3fbfa9
rahul <rahul.rana@codobux.in>
2025-12-20 11:21:56 +0530

    updated code

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -186,8 +186,8 @@ const userServices = {
           goodness: [],
         }
       );
-      const allProducts = await Model.Product.find({}).sort({ createdAt: -1 });
-      const dairyProduts = await Model.Product.find({product_type:{$in:["Milk","Flour"]}}).sort({createdAt: -1})
+      const allProducts = await Model.Product.find({product_type:{$nin:["Milk","Flour"]}}).sort({ createdAt: -1 }).limit(10);
+      const dairyProduts = await Model.Product.find({product_type:{$in:["Milk","Flour"]}}).sort({createdAt: -1}).limit(10)
       return successRes(res, 200, "Home screen data fetched successfully", {
         custom: boxesFilter.custom,
         goodness: boxesFilter.goodness,
